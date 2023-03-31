5f5fdcc74a1e3c46b3739b307042e4ce72009c89
Mitchell Gantz <mgantz22@icloud.com>
2023-03-31 09:33:27 -0700

    Add Animals model

---
+++ AnimalHouseApi/Models/Animals.cs
@@ -0,0 +1,11 @@
+namespace AnimalHouseApi.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public string SubSpecies { get; set; }
+    public int Age { get; set; }
+  }
+}
